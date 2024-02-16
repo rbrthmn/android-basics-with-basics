3b465b040c6f21c3c9dac798cf894ae7ff06faa5
rbrthmn <robertokenzohamano@gmail.com>
2024-02-16 04:49:05 -0300

    making images dynamical

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.androidbasics
 
-import android.graphics.ColorSpace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
@@ -13,16 +12,18 @@ import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.shadow
@@ -30,7 +31,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
@@ -75,27 +75,41 @@ class MainActivity : ComponentActivity() {
             artName = stringResource(R.string.art4_name),
             artYear = stringResource(R.string.art4_year)
         )
-        val artsPieces = listOf(art1, art2, art3, art4)
+        val artPieces = listOf(art1, art2, art3, art4)
+        var currentArt by remember { mutableIntStateOf(0) }
 
         Surface(modifier = modifier.fillMaxSize()) {
             Column(
                 horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround,
+                verticalArrangement = Arrangement.SpaceBetween,
                 modifier = modifier.verticalScroll(rememberScrollState())
             ) {
                 Column(
                     horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
+                    verticalArrangement = Arrangement.Center,
+                    modifier = modifier.padding(top = 40.dp)
                 ) {
-                    ArtImage(imageId = art1.imageId, imageName = art1.artName, modifier = modifier)
+                    ArtImage(
+                        imageId = artPieces[currentArt].imageId,
+                        imageName = artPieces[currentArt].artName,
+                        modifier = modifier
+                    )
                     ArtistInfo(
-                        artName = art1.artName,
-                        artistName = art1.artistName,
-                        artYear = art1.artYear,
+                        artName = artPieces[currentArt].artName,
+                        artistName = artPieces[currentArt].artistName,
+                        artYear = artPieces[currentArt].artYear,
                         modifier = modifier
                     )
                 }
-                ActionButtons(onNextAction = {}, onPreviousAction = {}, modifier = modifier)
+                ActionButtons(onNextAction = {
+                    if (currentArt == artPieces.lastIndex) {
+                        currentArt = 0
+                    } else currentArt++
+                }, onPreviousAction = {
+                    if (currentArt == 0) {
+                        currentArt = artPieces.lastIndex
+                    } else currentArt--
+                }, modifier = modifier)
             }
         }
     }
@@ -153,10 +167,10 @@ class MainActivity : ComponentActivity() {
                 .fillMaxWidth()
                 .padding(horizontal = 20.dp, vertical = 5.dp)
         ) {
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = { onPreviousAction() }) {
                 Text(text = stringResource(id = R.string.previous))
             }
-            Button(onClick = { /*TODO*/ }) {
+            Button(onClick = { onNextAction() }) {
                 Text(text = stringResource(id = R.string.next))
             }
         }
