8c3d0303b5d2cdd0cf12c2ec9972ca825872ae9c
rbrthmn <robertokenzohamano@gmail.com>
2024-03-18 19:32:41 -0300

    adding all resources

--- app/src/main/java/com/example/androidbasics/ui/theme/Type.kt
+++ app/src/main/java/com/example/androidbasics/ui/theme/Type.kt
@@ -17,49 +17,81 @@ val Montserrat = FontFamily(
     Font(R.font.montserrat_bold, FontWeight.Bold)
 )
 
+val Cabin = FontFamily(
+    Font(R.font.cabin_regular, FontWeight.Normal),
+    Font(R.font.cabin_bold, FontWeight.Bold)
+)
+
+// Set of Material typography styles to start with
+//val Typography = Typography(
+//    displayLarge = TextStyle(
+//        fontFamily = AbrilFatface,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 36.sp
+//    ),
+//    displayMedium = TextStyle(
+//        fontFamily = Montserrat,
+//        fontWeight = FontWeight.Bold,
+//        fontSize = 20.sp
+//    ),
+//    labelSmall = TextStyle(
+//        fontFamily = Montserrat,
+//        fontWeight = FontWeight.Bold,
+//        fontSize = 14.sp
+//    ),
+//    bodyLarge = TextStyle(
+//        fontFamily = Montserrat,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 14.sp
+//    )
+////        bodyLarge = TextStyle(
+////                fontFamily = FontFamily.Default,
+////                fontWeight = FontWeight.Normal,
+////                fontSize = 16.sp,
+////                lineHeight = 24.sp,
+////                letterSpacing = 0.5.sp
+////        )
+//        /* Other default text styles to override
+//    titleLarge = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.Normal,
+//        fontSize = 22.sp,
+//        lineHeight = 28.sp,
+//        letterSpacing = 0.sp
+//    ),
+//    labelSmall = TextStyle(
+//        fontFamily = FontFamily.Default,
+//        fontWeight = FontWeight.Medium,
+//        fontSize = 11.sp,
+//        lineHeight = 16.sp,
+//        letterSpacing = 0.5.sp
+//    )
+//    */
+//)
+
+
 // Set of Material typography styles to start with
 val Typography = Typography(
-    displayLarge = TextStyle(
-        fontFamily = AbrilFatface,
+    bodyLarge = TextStyle(
+        fontFamily = Cabin,
         fontWeight = FontWeight.Normal,
-        fontSize = 36.sp
+        fontSize = 16.sp,
+        lineHeight = 24.sp,
+        letterSpacing = 0.5.sp
+    ),
+    displayLarge = TextStyle(
+        fontFamily = Cabin,
+        fontWeight = FontWeight.Normal,
+        fontSize = 30.sp
     ),
     displayMedium = TextStyle(
-        fontFamily = Montserrat,
+        fontFamily = Cabin,
         fontWeight = FontWeight.Bold,
         fontSize = 20.sp
     ),
-    labelSmall = TextStyle(
-        fontFamily = Montserrat,
+    displaySmall = TextStyle(
+        fontFamily = Cabin,
         fontWeight = FontWeight.Bold,
-        fontSize = 14.sp
-    ),
-    bodyLarge = TextStyle(
-        fontFamily = Montserrat,
-        fontWeight = FontWeight.Normal,
-        fontSize = 14.sp
+        fontSize = 20.sp
     )
-//        bodyLarge = TextStyle(
-//                fontFamily = FontFamily.Default,
-//                fontWeight = FontWeight.Normal,
-//                fontSize = 16.sp,
-//                lineHeight = 24.sp,
-//                letterSpacing = 0.5.sp
-//        )
-        /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
---
+++ app/src/main/java/com/example/androidbasics/unit3/model/Hero.kt
@@ -0,0 +1,3 @@
+package com.example.androidbasics.unit3.model
+
+data class Hero(val nameRes: Int, val descriptionRes: Int, val imageRes: Int)
