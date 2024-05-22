fd7a2cb100e4bef4f65a13da0f16806f28ab3f54
rbrthmn <robertokenzohamano@gmail.com>
2024-05-22 19:47:21 -0300

    adding navigation feature to app bar

--- app/src/main/java/com/example/androidbasics/unit4/ui/elements/CupcakeScreen.kt
+++ app/src/main/java/com/example/androidbasics/unit4/ui/elements/CupcakeScreen.kt
@@ -17,6 +17,7 @@ package com.example.androidbasics.unit4.ui.elements
 
 import android.content.Context
 import android.content.Intent
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -41,6 +42,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import com.example.androidbasics.R
 import com.example.androidbasics.unit4.data.DataSource
@@ -49,11 +51,11 @@ import com.example.cupcake.ui.OrderViewModel
 import com.example.cupcake.ui.SelectOptionScreen
 import com.example.cupcake.ui.StartOrderScreen
 
-enum class CupcakeScreen {
-    Start,
-    Flavor,
-    Pickup,
-    Summary
+enum class CupcakeScreen(@StringRes val title: Int) {
+    Start(title = R.string.cupcake_app_name),
+    Flavor(title = R.string.choose_flavor),
+    Pickup(title = R.string.choose_pickup_date),
+    Summary(title = R.string.order_summary)
 }
 
 /**
@@ -62,12 +64,13 @@ enum class CupcakeScreen {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CupcakeAppBar(
+    currentScreen: CupcakeScreen,
     canNavigateBack: Boolean,
     navigateUp: () -> Unit,
     modifier: Modifier = Modifier
 ) {
     TopAppBar(
-        title = { Text(stringResource(id = R.string.cupcake_app_name)) },
+        title = { Text(stringResource(id = currentScreen.title)) },
         colors = TopAppBarDefaults.mediumTopAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer
         ),
@@ -90,17 +93,21 @@ fun CupcakeApp(
     viewModel: OrderViewModel = viewModel(),
     navController: NavHostController = rememberNavController()
 ) {
+    val backStackEntry by navController.currentBackStackEntryAsState()
+    val currentScreen = CupcakeScreen.valueOf(
+        backStackEntry?.destination?.route ?: CupcakeScreen.Start.name
+    )
 
     Scaffold(
         topBar = {
             CupcakeAppBar(
-                canNavigateBack = false,
-                navigateUp = { /* TODO: implement back navigation */ }
+                currentScreen = currentScreen,
+                canNavigateBack = currentScreen != CupcakeScreen.Start,
+                navigateUp = { navController.navigateUp() }
             )
         }
     ) { innerPadding ->
         val uiState by viewModel.uiState.collectAsState()
-
         NavHost(
             navController = navController,
             startDestination = CupcakeScreen.Start.name,
@@ -163,7 +170,8 @@ fun CupcakeApp(
                             navController
                         )
                     },
-                    onSendButtonClicked = { subject: String, summary: String -> shareOrder(context, subject, summary)
+                    onSendButtonClicked = { subject: String, summary: String ->
+                        shareOrder(context, subject, summary)
                     },
                     modifier = Modifier.fillMaxHeight()
                 )
