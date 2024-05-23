685477c0d0484d69f808a9e29d0f8b582842d24e
rbrthmn <robertokenzohamano@gmail.com>
2024-05-22 21:18:48 -0300

    creating navigation tests

---
+++ app/src/androidTest/java/com/example/androidbasics/unit4/ScreenAssertions.kt
@@ -0,0 +1,8 @@
+package com.example.androidbasics.unit4
+
+import androidx.navigation.NavController
+import org.junit.Assert.assertEquals
+
+fun NavController.assertCurrentRouteName(expectedRouteName: String) {
+    assertEquals(expectedRouteName, currentBackStackEntry?.destination?.route)
+}
