eb970ae92d3863aa98271fa157b8245d99036226
rbrthmn <robertokenzohamano@gmail.com>
2024-06-19 11:45:45 -0300

    adding files of the new project

--- app/src/main/java/com/example/androidbasics/MainActivity.kt
+++ app/src/main/java/com/example/androidbasics/MainActivity.kt
@@ -4,14 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.androidbasics.unit4.ui.elements.SportsApp
 import com.example.androidbasics.unit4.ui.theme.SportsTheme
+import com.example.racetracker.ui.RaceTrackerApp
+import com.example.racetracker.ui.theme.RaceTrackerTheme
 
 class MainActivity : ComponentActivity() {
     @OptIn(ExperimentalMaterial3WindowSizeClassApi::class)
@@ -19,41 +23,13 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
         setContent {
-            val screenSize = calculateWindowSizeClass(activity = this)
-            SportsTheme {
-                Surface {
-                    SportsApp(screenSize = screenSize.widthSizeClass)
+            RaceTrackerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                ) {
+                    RaceTrackerApp()
                 }
-            }
-        }
-    }
-
-    @Preview
-    @Composable
-    fun SportsAppCompactPreview() {
-        SportsTheme {
-            Surface {
-                SportsApp(screenSize = WindowWidthSizeClass.Compact)
-            }
-        }
-    }
-
-    @Preview(widthDp = 700)
-    @Composable
-    fun SportsAppMediumPreview() {
-        SportsTheme {
-            Surface {
-                SportsApp(screenSize = WindowWidthSizeClass.Medium)
-            }
-        }
-    }
-
-    @Preview(widthDp = 1000)
-    @Composable
-    fun SportsAppExpandedPreview() {
-        SportsTheme {
-            Surface {
-                SportsApp(screenSize = WindowWidthSizeClass.Expanded)
             }
         }
     }
