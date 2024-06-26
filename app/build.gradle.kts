df8aa86dbf50dced387a9bb243c30352c7fb1ab7
rbrthmn <robertokenzohamano@gmail.com>
2024-06-25 23:04:18 -0300

    changing serialization converter dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,8 +66,8 @@ dependencies {
     implementation("androidx.compose.material3:material3-window-size-class")
     implementation("androidx.compose.ui:ui-test-junit4-android:1.6.7")
     implementation("com.squareup.retrofit2:retrofit:2.9.0")
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("com.squareup.okhttp3:okhttp:4.11.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
 
     testImplementation("junit:junit:4.13.2")
