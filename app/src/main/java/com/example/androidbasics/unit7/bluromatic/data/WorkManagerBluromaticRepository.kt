51e16160327b4df55ddfe43f86a018b7c3c40f7d
rbrthmn <robertokenzohamano@gmail.com>
2024-08-31 23:10:52 -0300

    adding low battery constraint

--- app/src/main/java/com/example/androidbasics/unit7/bluromatic/data/WorkManagerBluromaticRepository.kt
+++ app/src/main/java/com/example/androidbasics/unit7/bluromatic/data/WorkManagerBluromaticRepository.kt
@@ -19,6 +19,7 @@ package com.example.androidbasics.unit7.bluromatic.data
 import android.content.Context
 import android.net.Uri
 import androidx.lifecycle.asFlow
+import androidx.work.Constraints
 import androidx.work.Data
 import androidx.work.ExistingWorkPolicy
 import androidx.work.OneTimeWorkRequest
@@ -57,8 +58,12 @@ class WorkManagerBluromaticRepository(context: Context) : BluromaticRepository {
             ExistingWorkPolicy.REPLACE,
             OneTimeWorkRequest.from(CleanupWorker::class.java)
         )
+        val constraints = Constraints.Builder()
+            .setRequiresBatteryNotLow(true)
+            .build()
         val blurBuilder = OneTimeWorkRequestBuilder<BlurWorker>()
         blurBuilder.setInputData(createInputDataForWorkRequest(blurLevel, imageUri))
+        blurBuilder.setConstraints(constraints)
         continuation = continuation.then(blurBuilder.build())
         val save = OneTimeWorkRequestBuilder<SaveImageToFileWorker>()
             .addTag(TAG_OUTPUT)
