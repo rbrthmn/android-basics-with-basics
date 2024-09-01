ad46b1f0e06257cd53c8a486d5cb157b5146d9ae
rbrthmn <robertokenzohamano@gmail.com>
2024-08-31 23:09:31 -0300

    adding option to cancel blurring image

--- app/src/main/java/com/example/androidbasics/unit7/bluromatic/ui/BlurViewModel.kt
+++ app/src/main/java/com/example/androidbasics/unit7/bluromatic/ui/BlurViewModel.kt
@@ -82,6 +82,10 @@ class BlurViewModel(private val bluromaticRepository: BluromaticRepository) : Vi
             }
         }
     }
+
+    fun cancelWork() {
+        bluromaticRepository.cancelWork()
+    }
 }
 
 sealed interface BlurUiState {
